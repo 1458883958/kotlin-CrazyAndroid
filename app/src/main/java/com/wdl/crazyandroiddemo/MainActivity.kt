0b9700a6bf5ea20e86b1876333cc43ad76b7582c
1458883958 <32236003+1458883958@users.noreply.github.com>
2018-10-29 20:29:07 +0800

    代码中控制UI

--- app/src/main/java/com/wdl/crazyandroiddemo/MainActivity.kt
+++ app/src/main/java/com/wdl/crazyandroiddemo/MainActivity.kt
@@ -2,11 +2,30 @@ package com.wdl.crazyandroiddemo
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.view.ViewGroup
+import android.widget.Button
+import android.widget.LinearLayout
+import android.widget.TextView
+import java.util.*
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        //创建一个线性布局管理器
+        val linearLayout = LinearLayout(this)
+        setContentView(linearLayout)
+        linearLayout.orientation = LinearLayout.VERTICAL
+        val show = TextView(this)
+        show.text = "hello kotlin"
+        val bn = Button(this)
+        bn.layoutParams = ViewGroup.LayoutParams(ViewGroup.LayoutParams.WRAP_CONTENT,ViewGroup.LayoutParams.WRAP_CONTENT)
+        bn.text = "点击"
+        linearLayout.addView(show)
+        linearLayout.addView(bn)
+        bn.setOnClickListener {
+            show.text = "onClick() + ${Date()}"
+        }
+
     }
 }
